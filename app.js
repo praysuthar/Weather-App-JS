94828e29d6f2c227dbf82fba8038174e3f80e01a
Pray Suthar <praysuthar513@gmail.com>
2023-11-10 17:38:51 -0500

    Clean Code

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ function getWeatherData() {
 	const apiKey = "1f37ca8126300c1de3c08e964e37f71a";
 	const city = document.getElementById("city").value;
 	const weather = document.getElementById("weather");
-	// const wi = document.getElementById("w");
 	fetch(
 		`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 	)
@@ -10,13 +9,9 @@ function getWeatherData() {
 		.then((data) => {
 			const temp = Math.round(data.main.temp);
 			const time = new Date(data.dt * 1_000);
-			// const temp_icon = data.weather[0].icon;
 			const description = data.weather[0].description;
 			const humidity = data.main.humidity;
 			const windspeed = data.wind.speed;
-			// const weatherIcon = ` https://openweathermap.org/img/wn/${temp_icon}@2x.png`;
-			// document.getElementById("weatherIcon").appendChild(img);
-			// w.src = weatherIcon;
 			weather.innerHTML = `
 			<h1> ${temp}°C </h1>
 			<p> Description: ${description} </p>
